f298441a558e1ce967a9584ca61a80fdf4466fc9
LucasDevSystem <highspeedsetup@gmail.com>
2024-10-02 23:27:08 -0300

    Fix no

--- src/Arvore/No.java
+++ src/Arvore/No.java
@@ -3,24 +3,14 @@ package Arvore;
 import java.util.ArrayList;
 import java.util.List;
 
-import Data.Palavra;
 
 public class No {
-  int linha;
-  String palavra;
-  List<Integer> linhas = new ArrayList<>();
-  Palavra word;
+  int valor;
   No direita;
   No esquerda;
 
-  public No(String palavraInicial, int valorInicial) {
-    this.linha = valorInicial;
-    this.palavra = palavraInicial;
-    this.linhas.add(valorInicial);
-  }
-
-  public void adicionarLinha(int linha) {
-    this.linhas.add(linha);
+  public No(int valor) {
+    this.valor = valor;
   }
 
 }
